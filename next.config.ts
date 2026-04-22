b66ac9f31e76ff9790760230cd9428adccaa1eb7
Manavraj-0 <miturajvansh123456@gmail.com>
2026-04-22 22:16:03 +0530

    Remove output and basePath configurations from Next.js config

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: 'export',
   images: {
     unoptimized: true,
   },
-  basePath: '/receptionistai',
 };
 
 export default nextConfig;
